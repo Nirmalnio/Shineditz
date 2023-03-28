4a8f3de19d6a1449ed29cf696c7d1c76f2e1d85f
nirmalnio <nirmalnio123@gmail.com>
2023-03-29 03:06:43 +0530

    homepage added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import Logo from '../../Assets/Images/Shine Logo 3.jpg'
+import { Link } from 'react-router-dom'
 function Footer() {
   return (
-    <div>
-      
+    <div className='footermaindiv'>
+
+      <div className='footerinside'>
+           <div>
+                <img src={Logo} alt="" className='footerlogo'/>
+           </div>
+
+           <div className='footeroptions'>
+            <div>
+              <b>Product</b>
+              <span></span>
+            </div>
+
+            <div>
+              <b>Company</b>
+
+            </div>
+
+            <div>
+                <b>Legal</b>
+            </div>
+
+           </div>
+      </div>
+
     </div>
   )
 }
