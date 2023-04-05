382528b95915b7853f96e6730d92f9424ee083f5
rubanshiva <rubanshivaram@gmail.com>
2023-04-05 17:11:08 +0530

    pricing page

--- src/pages/pricing/pricing.jsx
+++ src/pages/pricing/pricing.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import Header from "../../Components/Head/Header";
 import './pricing.css';
-import basic from "../../Assets/Images/bag.jpg";
+import bag from "../../Assets/Images/bag.jpg";
+import schoolbag from "../../Assets/Images/schoolbag.jpg";
+import earings from "../../Assets/Images/earrings.jpg";
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import {BiRupee} from "react-icons/bi";
+
 
 
 function Pricing() {
+    const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(0),
+        textAlign: 'center',
+        borderRadius:'10',
+        color: theme.palette.text.secondary,
+    }));
+
 
     return (
         <div>
@@ -17,7 +34,7 @@ function Pricing() {
                 </div>
                 <div className="cardsec">
                     <div>
-                        <button className="buttoncards">Clipping path</button>
+                        <button className="buttoncardsactive">Clipping path</button>
                     </div>
                     <div>
                         <button className="buttoncards">Image Masking</button>
@@ -34,24 +51,43 @@ function Pricing() {
                     <div>
                         <button className="buttoncards">Ghost Mannequin</button>
                     </div>
+                </div>                
+                <div className="gridbox">
+                    <Grid container spacing={0} mt={10} mb={5} mx={4}>
+                        <Grid item xs={4} md={4}>
+                            <Item>
+                                <p className="gridtitle">Basic Image</p>
+                                <p><span className="basic"><BiRupee/>0.30</span>
+                                <div className="smalltxt">per image</div></p>
+                                <img src={bag}></img>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                        <Item>
+                                <p className="gridtitle">Medium Image</p>
+                                <p><span className="basic"><BiRupee/>1.50</span>
+                                <div className="smalltxt">per image</div></p>
+                                <img src={schoolbag}></img>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={4} md={4}>
+                        <Item>
+                                <p className="gridtitle">Complex Image</p>
+                                <p><span className="basic"><BiRupee/>3.50</span>
+                                <div className="smalltxt">per image</div></p>
+                                <img src={earings}></img>
+                            </Item>
+                        </Grid>
+
+                    </Grid>
                 </div>
-                <div className="card2">
-                    <div>
-                        <p>Basic Image</p>
-                        <p>0.30<small>per image</small> </p>
-                        <img src={basic}></img>
-                    </div>
-                    <div>
-                        <p>Basic Image</p>
-                        <p>0.30<small>per image</small> </p>
-                        <img src={basic}></img>
-                    </div>
-
+                <div className="submitbutton" >
+                   <div> <button className='button3' type="submit">View Samples</button></div>
+                   <div> <button className='button1' type="submit">Contact US</button></div>
                 </div>
-                <button className='button2' type="submit">View Samples</button>
-
-                <button className='button1' type="submit">Contact US</button>
-
+            </div>
+            <div>
+                <h1 style={{marginBottom:100}} className="headdiv ">Frequently Asked Questions</h1>
             </div>
         </div>
     )
